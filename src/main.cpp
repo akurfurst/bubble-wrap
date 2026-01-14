f71a9136c5ace8b98478e27df13b78b510553b50
akurfurst <219035144+akurfurst@users.noreply.github.com>
2026-01-14 09:44:06 +0000

    Made the background change while button is held, not presed and added a new background for when both buttons are held

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,19 @@ int main(){
     bn::backdrop::set_color(bn::color(20,20,31));
 
     while(true){
-        if(bn::keypad::a_pressed())
+        if (bn::keypad::a_held() && bn::keypad::b_held())
+        {
+            bn::backdrop::set_color(bn::color(0, 0, 31));
+        }
+        else if(bn::keypad::a_held())
         {
             bn::backdrop::set_color(bn::color(31, 21, 22));
         }
-
-        if(bn::keypad::b_pressed()){
+        else if(bn::keypad::b_held()){
             bn::backdrop::set_color(bn::color(22, 31, 21));
+        }
+        else{
+            bn::backdrop::set_color(bn::color(20, 20, 31));
         }
 
         bn::core::update();
