2159ca3378ae85d8af9c9da90b97199fcf116db6
akurfurst <219035144+akurfurst@users.noreply.github.com>
2026-01-14 09:35:15 +0000

    Added user inputs to change background color using a and b buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
+#include <bn_keypad.h>
+
 
 int main(){
     bn::core::init();
     bn::backdrop::set_color(bn::color(20,20,31));
+
     while(true){
+        if(bn::keypad::a_pressed())
+        {
+            bn::backdrop::set_color(bn::color(31, 21, 22));
+        }
+
+        if(bn::keypad::b_pressed()){
+            bn::backdrop::set_color(bn::color(22, 31, 21));
+        }
+
         bn::core::update();
     }
 }
