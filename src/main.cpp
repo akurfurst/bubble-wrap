dceb6f16c758c141ffd18b034146631ed0e15500
akurfurst <219035144+akurfurst@users.noreply.github.com>
2026-01-20 04:11:57 +0000

    Added sprites

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,19 @@
 #include <bn_color.h>
 #include <bn_core.h>
 #include <bn_keypad.h>
-
+#include <bn_sprite_ptr.h>
+#include <bn_sprite_items_dot.h>
 
 int main(){
     bn::core::init();
     bn::backdrop::set_color(bn::color(20,20,31));
+
+    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(10,40);
+    bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-10, 40);
+    bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(-10, -40);
+    bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(10, -40);
+    bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(40, 0);
+    bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(-40, 0);
 
     while(true){
         if (bn::keypad::a_held() && bn::keypad::b_held())
